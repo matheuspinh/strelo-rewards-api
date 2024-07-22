80a1d39460315af702aa89ae5565c3b521bbbd14
Matheus Pinheiro <matheus.rj.pinheiro@gmail.com>
2024-07-22 17:49:00 -0300

    chore: Update User and Company models in Prisma schema and add seed

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { hash } from "bcryptjs";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const password_hash = await hash('123123123', 6);
+  const company = await prisma.company.upsert({
+    where: { name: 'strelo' },
+    update: {},
+    create: {
+      name: 'strelo',
+      users: {
+        create: {
+          email: 'admin@admin.com',
+          passwordHash: password_hash,
+          username: 'admin',
+        }
+      },
+      description: 'A company that helps other companies to grow',
+    }
+  })
+}
+
+main().catch((error) => {
+  console.error('Error seeding', error);
+  process.exit(1);
+});
