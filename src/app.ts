5e558f39d0fdbba1d20956c68e4660476651ae73
Matheus Pinheiro <matheus.rj.pinheiro@gmail.com>
2024-07-22 18:58:10 -0300

    feat: app CORS setup and errorHandler creation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,14 @@
 import fastify from 'fastify'
-import { ZodError } from 'zod'
 import { env } from './env'
 import fastifyJwt from '@fastify/jwt'
 import fastifyCookie from '@fastify/cookie'
+import fastifyCors from '@fastify/cors'
 import { usersRoutes } from '@/http/controllers/users/routes'
+import { errorHandler } from './http/middlewares/error-handler'
 
 export const app = fastify()
+
+app.register(fastifyCors)
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
@@ -22,18 +25,4 @@ app.register(fastifyCookie)
 
 app.register(usersRoutes)
 
-app.setErrorHandler((error, _, reply) => {
-  if (error instanceof ZodError) {
-    return reply
-      .status(400)
-      .send({ message: 'Validation error', issues: error.format() })
-  }
-
-  if (env.NODE_ENV !== 'production') {
-    console.error(error)
-  } else {
-    // ToDo: Here we should log to an external tool like DataDog/NewRelic/Sentry
-  }
-
-  return reply.status(500).send({ message: 'Internal server error.' })
-})
+app.setErrorHandler(errorHandler)
