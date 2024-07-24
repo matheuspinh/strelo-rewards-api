de703f67929fdaacffbe023e731a1f76a5d0f485
Matheus Pinheiro <matheus.rj.pinheiro@gmail.com>
2024-07-24 11:36:50 -0300

    feat: Add makeListUsersService factory function

---
+++ src/services/users/list-users.ts
@@ -0,0 +1,27 @@
+import { UsersRepository } from "@/repositories/users-repository"
+import { UserUnauthorizedError } from "../errors/user-errors"
+import { ResourceNotFound } from "../errors/general-errors"
+
+export interface ListUsersData {
+  companyId: string,
+  userId: string
+}
+
+export class ListUsersService {
+  constructor(private usersRepository: UsersRepository) { }
+
+  async execute(data: ListUsersData) {
+    const user = await this.usersRepository.findById(data.userId)
+    if (!user) {
+      throw new ResourceNotFound('Usuário não encontrado')
+    }
+
+    if (user.role !== 'admin') {
+      throw new UserUnauthorizedError()
+    }
+
+    const users = await (await this.usersRepository.listByCompanyId(data.companyId)).filter((user) => user.id !== data.userId)
+
+    return users
+  }
+}
