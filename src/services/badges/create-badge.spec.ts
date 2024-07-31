bb4c8dfce087005e2cead082e1072e5f0a7745f7
Matheus Pinheiro <matheus.rj.pinheiro@gmail.com>
2024-07-30 22:19:19 -0300

    feat: Add badge routes and createBadge controller

---
+++ src/services/badges/create-badge.spec.ts
@@ -0,0 +1,45 @@
+import { FakeBadgesRepository } from "@/repositories/fakes/fake-badges-repository";
+import { beforeEach, describe, expect, it } from "vitest";
+import { CreateBadgeService } from "./create-badge";
+import { FakeCompanyRepository } from "@/repositories/fakes/fake-company-repository";
+
+let badgesRepository: FakeBadgesRepository
+let companiesRepository: FakeCompanyRepository
+let sut: CreateBadgeService
+
+describe("Create Badge Service", () => {
+  beforeEach(() => {
+    companiesRepository = new FakeCompanyRepository()
+    badgesRepository = new FakeBadgesRepository()
+    sut = new CreateBadgeService(badgesRepository, companiesRepository)
+  })
+
+  it("should be able to create a badge", async () => {
+    const company = await companiesRepository.create({
+      name: "Company",
+      description: "Company description"
+    })
+
+    const badge = await sut.execute({
+      companyId: company.id,
+      title: "Badge Title",
+      description: "Badge Description",
+      imageUrl: null,
+      earnedByIDs: [],
+      missionsIDs: []
+    })
+
+    expect(badge.title).toEqual('Badge Title')
+  })
+
+  it("should throw an error if company does not exist", async () => {
+    await expect(sut.execute({
+      companyId: "invalid-company-id",
+      title: "Badge Title",
+      description: "Badge Description",
+      imageUrl: null,
+      earnedByIDs: [],
+      missionsIDs: []
+    })).rejects.toThrow('Empresa não encontrada')
+  })
+})
---
+++ src/services/badges/create-badge.ts
@@ -0,0 +1,46 @@
+import { BadgesRepository } from "@/repositories/badges-repository";
+import { DatabaseError, ResourceNotFound } from "../errors/general-errors";
+import { CompanyRepository } from "@/repositories/company-repository";
+import { MultipartFile } from "@fastify/multipart";
+import uploadImageToBucket from "@/lib/upload-image-to-bucket";
+
+export interface CreateBadgeData {
+  title: string
+  description: string
+  companyId: string
+  image?: MultipartFile
+}
+
+export class CreateBadgeService {
+  constructor(private badgesRepository: BadgesRepository, private companiesRepository: CompanyRepository) { }
+
+  async execute(data: CreateBadgeData) {
+    const company = await this.companiesRepository.findById(data.companyId)
+
+    if (!company) {
+      throw new ResourceNotFound('Empresa não encontrada')
+    }
+
+    let imageUrl
+
+    if (data.image) {
+      try {
+        imageUrl = await uploadImageToBucket.execute({ address: 'badges', file: data.image, userId: data.title })
+      } catch (error) {
+        throw new Error('Erro ao fazer upload da imagem')
+      }
+    }
+
+    try {
+      const badge = await this.badgesRepository.create({
+        title: data.title,
+        description: data.description,
+        companyId: data.companyId,
+        imageUrl: imageUrl || null
+      })
+      return badge
+    } catch (error) {
+      throw new DatabaseError('Não foi possível criar a conquista')
+    }
+  }
+}
